20baf806293917c4d233a574c075acd80fdfe975
lephusang <lephusangit@gmail.com>
2017-05-13 10:31:07 +0700

    Create validateForm.js

---
+++ validateForm.js
@@ -0,0 +1,31 @@
+
+function validateForm()    
+  {
+   var hoten = document.forms['myForm']['entry.2050587799'].value;
+   var sdt = document.forms['myForm']['entry.2101061946'].value;
+   if(hoten == ''){
+    document.getElementById('error-message-hoten').innerHTML = 'Anh chị vui lòng điền họ tên <br />';
+    return false;
+   }
+   else 
+   {
+    document.getElementById('error-message-hoten').innerHTML = '';
+   }
+   if(sdt == '')
+   {
+    document.getElementById('error-message-sdt').innerHTML = 'Anh chị vui lòng nhập sđt <br />';
+    return false;
+   }
+   else if (isNaN(sdt))
+   {
+    document.getElementById('error-message-sdt').innerHTML = 'Anh chị đã nhập sai định dạng số điện thoại <br />';
+    return false;
+   }
+   else
+   {
+      document.getElementById('error-message-sdt').innerHTML = '';
+   }
+  
+   document.getElementById('success-message').innerHTML = ' <br /> <br />Cám ơn anh chị đã để lại thông tin, nhân viên bên em sẽ liên hệ với anh chị trong thời gian sớm nhất.<br />';
+   return true;
+}
